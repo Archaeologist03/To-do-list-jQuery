b345148034b5fd1207c516a7c06047270de393ae
Archaeologist03 <omniiknight@hotmial.com>
2018-09-21 11:36:23 +0200

    second commit - done with app

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,16 +1,27 @@
 //check of specific to dos by clicking
-$("li").on("click", function() {
-
-    $(this).toggleClass("completed");
-   
+$("ul").on("click", function(e) {
+    $(e.target).toggleClass("completed");
 })
 
 //click on X to delete to do
-$("span").on("click", function(e) {
+$("ul").on("click", "span", function(e) {
     $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
     
     e.stopPropagation();
+})
 
+//add new item on enter keypress
+$("input[type='text']").on("keypress", function(e) {
+    if(e.which === 13) {
+        let toDoText = $(this).val();
+        $(this).val(""); 
+        $("ul").append(`<li><span><i class="fa fa-trash"></i></span> ${toDoText}</li>`)
+    }
+})
+
+//toggle input on plus icon click
+$(".fa-plus").on("click", function() {
+    $("input[type='text']").fadeToggle();
 })
